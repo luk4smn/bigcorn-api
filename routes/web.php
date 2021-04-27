aa0af8d04088c5a4b3de721e00d6731006e52a66
lucas.nunes <lucas.mn@outlook.com>
2021-04-27 12:56:29 -0300

    test commit

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 	//return Redirect::to("perguntas.php");
 
-	include public_path().'\perguntas.php';
+	//include public_path().'\perguntas.php';
 
 	//return response()->file(public_path('perguntas.php'));
 
-    //return view('welcome');
+    return view('welcome');
 });
